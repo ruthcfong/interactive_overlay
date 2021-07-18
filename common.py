b384636a826bb3c8a6b968d24f31855e83f95a98
Ruth Fong <ruthcfong@gmail.com>
2021-07-18 17:04:41 -0400

    add functions for geometric transformations

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 import numpy as np
+import PIL
 
 
 YELLOW = (1., 1., 0.)
@@ -18,6 +19,12 @@ def cossim_grid(acts1, acts2):
     cossims = [[many_cossim(acts2, v)
                 for v in row] for row in acts1]
     return np.asarray(cossims)
+
+
+def get_cossim_grids(acts):
+  grids = [[np.hstack(np.hstack(cossim_grid(acts1, acts2)))
+          for acts2 in acts] for acts1 in acts]
+  return grids
 
 
 def add_color_index(arr, stride, color=YELLOW):
@@ -43,3 +50,54 @@ def add_color_index_multi(arr, stride, color_each=False, color=YELLOW):
             arr[i][j] = add_color_index(arr[i][j], stride, color=color)
     assert isinstance(arr[0][0], np.ndarray)
     return arr
+
+
+def polar2degrees(x):
+  return x*360/(2*math.pi)
+
+
+def get_rot_coords(x, y, a, N=0):
+    off = float((N-1)/2)
+    x_ = (y - off) * np.sin(a) + (x - off) * np.cos(a)
+    y_ = (y - off) * np.cos(a) - (x - off) * np.sin(a)
+    return x_ + off, y_ + off
+
+
+def get_scale_coords(x, y, N, curr_bbox, target_bbox, endpoint=True):
+  offset = -1 if endpoint else 0
+
+  v_global = y / float(N + offset) * (curr_bbox[2] - curr_bbox[0]) + curr_bbox[0]
+  u_global = x / float(N + offset) * (curr_bbox[3] - curr_bbox[1]) + curr_bbox[1]
+
+  # y_curr = (v_global - curr_bbox[0]) / (curr_bbox[2] - curr_bbox[0]) * float(N + offset)
+  # x_curr =  (u_global - curr_bbox[1]) / (curr_bbox[3] - curr_bbox[1]) * float(N + offset)
+  # assert(int(np.round(y_curr)) == y)
+  # assert(int(np.round(x_curr)) == x)
+
+  # if u_global < target_bbox[1] or u_global > target_bbox[3] or v_global < target_bbox[0] or v_global > target_bbox[2]:
+  #  return np.nan, np.nan
+  # else:
+  y_target = (v_global - target_bbox[0]) / (target_bbox[2] - target_bbox[0]) * float(N + offset)
+  x_target =  (u_global - target_bbox[1]) / (target_bbox[3] - target_bbox[1]) * float(N + offset)
+
+  return x_target, y_target
+
+
+def get_image_name(path):
+  return os.path.splitext(os.path.basename(path))[0]
+
+
+def create_dir_if_necessary(directory):
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
+def save_img(object, url, domain=None, **kwargs):
+    if isinstance(object, np.ndarray):
+        normalized = _normalize_array(object, domain=domain)
+        object = PIL.Image.fromarray(normalized)
+
+    if isinstance(object, PIL.Image.Image):
+        object.save(url, **kwargs)  # will infer format from url's url ext.
+    else:
+        raise ValueError("Can only save_img for numpy arrays or PIL.Images!")
--- utils_tensorflow.py
+++ utils_tensorflow.py
@@ -22,3 +22,39 @@ def get_acts(model, x, layer_name):
     T = render.import_model(model, t_input, t_input)
     acts = T(layer_name).eval({t_input: x})[0]
   return acts
+
+
+def get_acts_rotate(model, layer, img, rot_angle=0.0, interpolation='NEAREST'):
+  with tf.Graph().as_default(), tf.Session():
+    t_input = tf.placeholder(tf.float32, [224, 224, 3])
+    rot_input = tf.contrib.image.rotate(t_input, rot_angle,
+                                        interpolation=interpolation)
+    T = render.import_model(model, rot_input, rot_input)
+    acts = T(layer).eval({t_input: img})[0]
+    rot_img = rot_input.eval({t_input: img})
+  return acts, rot_img
+
+
+def get_acts_scale(model, layer, img, box, crop_size=[224, 224], interpolation='bilinear'):
+  img = img[np.newaxis, :, :, :]
+  with tf.Graph().as_default(), tf.Session():
+    t_input = tf.placeholder(tf.float32, [1, 224, 224, 3])
+    t_boxes = tf.placeholder(tf.float32, [1, 4])
+    t_box_ind = tf.placeholder(tf.int32, [1,])
+    t_crop_size = tf.placeholder(tf.int32, [2,])
+
+    box_ind = tf.constant([0], tf.int32)
+    transformed_input = tf.image.crop_and_resize(t_input,
+                                                 boxes=t_boxes,
+                                                 box_ind=box_ind,
+                                                 crop_size=crop_size,
+                                                 method=interpolation)
+    T = render.import_model(model, transformed_input, transformed_input)
+    feed_dict = {
+        t_input: img,
+        t_boxes: box,
+        t_crop_size: crop_size}
+    acts = T(layer).eval(feed_dict)[0]
+    transformed_img = transformed_input.eval(feed_dict)[0]
+  return acts, transformed_img
+
